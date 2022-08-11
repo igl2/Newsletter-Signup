90cd340beac7afdbc41e8ac4d5fd406a7fe95c21
Isaiah Lleva <isaiahlleva@gmail.com>
2022-08-10 22:22:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const dotenv = require('dotenv');
+dotenv.config();
+const key = process.env.key;
+const urlId = process.env.url;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/signup.html')
+});
+
+app.post('/', (req, res) => {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const url = urlId;
+    const options = {
+        method: 'POST',
+        auth: key
+    }
+    const request = https.request(url, options, (response) => {
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        } else {
+            res.sendFile(__dirname + '/failure.html');
+        }
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+    
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+});
+
+app.post("/success", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port 3000");
+});
